52203f33a1fbe2468d347a3fb2d426adddba5083
Pierce Boyack <piercebo@buffalo.edu>
2022-07-16 03:09:56 -0400

    Updates

---
+++ precache-manifest.99a3d30970769054b5000929f295eec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da3ff354e77c20119acb",
+    "url": "/reactQuote/static/css/main.f147110c.chunk.css"
+  },
+  {
+    "revision": "da3ff354e77c20119acb",
+    "url": "/reactQuote/static/js/main.da3ff354.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/reactQuote/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "f59cb15d6c8b21faae69",
+    "url": "/reactQuote/static/js/2.f59cb15d.chunk.js"
+  },
+  {
+    "revision": "00af000b6040f17fa19e",
+    "url": "/reactQuote/static/js/runtime~main.00af000b.js"
+  },
+  {
+    "revision": "6c4350256ff29bfa3ddd6bcb20946c57",
+    "url": "/reactQuote/index.html"
+  }
+];
